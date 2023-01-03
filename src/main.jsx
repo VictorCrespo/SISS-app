54caa7fbfd8c75645dca0690b10eefe60c10d424
VictorCrespo <VictorCrespo.Developer@gmail.com>
2023-01-02 21:21:22 -0800

    Se quito el encabezado

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom/client'
 import { Login } from './components/login/Login'
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <h1>Hola</h1>
     <Login/>
   </React.StrictMode>,
 )
