c39b5703e2501b24852436e0d54752a01175cf56
VictorCrespo <VictorCrespo.Developer@gmail.com>
2023-01-25 00:27:54 -0800

    se agrego el menu para seleccionar los programas

--- src/main.jsx
+++ src/main.jsx
@@ -2,8 +2,9 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { Login } from './components/login/Login'
 import {LoginRK} from './components/login/LoginRK'
+import { Programas } from './components/programas/Programas'
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <LoginRK/>
+    <Programas/>
   </React.StrictMode>,
 )
