795b1d5a939ef145109bcbabb49c4fac5de0065c
VictorCrespo <VictorCrespo.Developer@gmail.com>
2023-02-27 22:35:53 -0800

    Se corrigio un error donde se actulizaba la informacion

--- src/components/inscripcion/Inscripcion.jsx
+++ src/components/inscripcion/Inscripcion.jsx
@@ -70,7 +70,8 @@ export function Inscripcion(){
                 setMensaje(mensaje);
                 setOpenMensaje(true);
                 setExpandedCard(null);
-                setEmergente(false)
+                setEmergente(false);
+                getAlumnosProgramas();
             } catch (error) {
                 setMensajeError(error.message);
                 setOpenError(true);
@@ -89,7 +90,8 @@ export function Inscripcion(){
                 setMensaje(mensaje);
                 setOpenMensaje(true);
                 setExpandedCard(null);
-                setEmergente(false)
+                setEmergente(false);
+                getAlumnosProgramas();
             } catch (error) {
                 setMensajeError(error.message);
                 setOpenError(true);
