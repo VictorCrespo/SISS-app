b42f94fe78bf94e4ee1340edd6c4dab77f477069
VictorCrespo <VictorCrespo.Developer@gmail.com>
2023-02-28 01:50:38 -0800

    Se restructuro el login y se agrego la clase para tokens

--- src/components/login/LoginRK.jsx
+++ src/components/login/LoginRK.jsx
@@ -1,85 +1,150 @@
-import Box from '@mui/material/Box';
-import Button from '@mui/material/Button';
-import FormControl from '@mui/material/FormControl'
-import Grid from '@mui/material/Grid';
-import IconButton from '@mui/material/IconButton'
-import InputAdornment from '@mui/material/InputAdornment'
-import InputLabel from '@mui/material/InputLabel'
-import Link from "@mui/material/Link"
-import OutlinedInput from '@mui/material/OutlinedInput'
-import TextField from '@mui/material/TextField';
-import Typography from '@mui/material/Typography'
+import { useState } from 'react';
 
-import * as React from 'react';
+import PropTypes from 'prop-types';
 
-import ThemeProvider from "@mui/material/styles/ThemeProvider"
-import Visibility from '@mui/icons-material/Visibility'
-import VisibilityOff from '@mui/icons-material/VisibilityOff';
+import {
+    Box,
+    Button,
+    Card,
+    FormControl,
+    Grid,
+    IconButton,
+    InputAdornment,
+    InputLabel,
+    Link,
+    OutlinedInput,
+    Tab,
+    Tabs,
+    TextField,
+    Typography
+} from '@mui/material'
 
-import {Purple,Gray} from "../themes/themecofig"
+import {
+    Visibility,
+    VisibilityOff
+} from '@mui/icons-material'
 
 import Background from './images/login.jpg'
 import Logo from './images/logo.png'
 
+function TabPanel(props) {
 
-export function LoginRK(){
-    const [showPassword, setShowPassword] = React.useState(false);
+    const { children, value, index, ...other } = props;
 
-    const handleClickShowPassword = () => setShowPassword((show) => !show);
+    return (
+        <div
+            role="tabpanel"
+            hidden={value !== index}
+            id={`simple-tabpanel-${index}`}
+            aria-labelledby={`simple-tab-${index}`}
+            {...other}
+        >
+            {value === index && (
+                <Box sx={{ p: 3 }}>
+                    {children}
+                </Box>
+            )}
+        </div>
+    );
+}
+
+TabPanel.propTypes = {
+    children: PropTypes.node,
+    index: PropTypes.number.isRequired,
+    value: PropTypes.number.isRequired,
+};
+
+function a11yProps(index) {
+    return {
+        id: `simple-tab-${index}`,
+        'aria-controls': `simple-tabpanel-${index}`,
+    };
+}
+
+async function loginUser(credentials) {
+    
+    console.log(credentials)
+
+    return fetch('http://localhost:8080/login', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(credentials)
+    }).then(data => data.json())
+}
+
+export function LoginRK({ setToken }){
+
+    const [opciones, setOpciones] = useState(0);
+
+    const [vercontrasenia, setVerContrasenia] = useState(false);
+
+    const [usuario,setUsuario] = useState('');
+
+    const [contrasenia,setContrasenia] = useState('');
+
+    const cambiarTabs = (event, newValue) => {
+        setOpciones(newValue);
+    };
+
+    const handleClickShowPassword = () => setVerContrasenia((ver) => !ver);
 
     const handleMouseDownPassword = (event) => {
         event.preventDefault();
     };
 
+    const submit = async () => {
+        const token = await loginUser({
+            'Nombre':usuario,
+            'Contrasena':contrasenia
+        });
+        setToken(token);
+    };
+
     return(
-        <Box sx={{ flexGrow: 1}}>
-            <Grid  
-                container
-                direction="row"
-            >
-                <Grid item xs={7}>
-                    
+        <Box>
+            <Grid container>
+                <Grid item xs={6} sm={7}>
+                    <Box 
+                        height={"99.35vh"} 
+                        display={'flex'}
+                        alignItems={'center'}    
+                        justifyContent={'center'}
+                        sx={{
+                            backgroundImage: 'url('+Background+')', 
+                            backgroundSize: 'cover',
+                            backgroundPosition: 'center',
+                        }}
+                    >
+                        <img  height={300}src={Logo}/>
+                    </Box>
                 </Grid>
-                <Grid item xs={5}>
-                    <Box>
-                        <Box 
-                            display={"flex"}
-                            marginTop={5}
-                            marginLeft={5}
-                        >
-                            <ThemeProvider theme={Purple}>
-                                <Button variant='contained'>Crear cuenta</Button>
-                            </ThemeProvider>
-                        </Box>
-                        <Box
-                            marginTop={10}
-                            borderRadius={5}
-                            marginLeft={5}
-                            marginRight={5}
-                            boxShadow={" 10px 10px 10px 10px #ccc"}
-                        >
-                            <Box
-                                display={"flex"}
-                                flexDirection={"column"}
-                                alignItems={"center"}
-                                justifyContent={"center"}
-                                paddingTop={5}
-                            >
-                                <Typography variant='h2'>Iniciar Sesion</Typography>
-                                <ThemeProvider theme={Purple}>
+                <Grid item xs={6} sm={5}>
+                    <Box display={'flex'} justifyContent={'center'} alignItems={'center'} sx={{height:"100%",backgroundColor: '#f4f6f6'}}>
+                        <Card  sx={{height: "50%",width:"70%"}}>
+                            <Box>
+                                <Tabs value={opciones} onChange={cambiarTabs}  centered>
+                                    <Tab label="Iniciar Sesion" {...a11yProps(0)} />
+                                    <Tab label="Registrarse" {...a11yProps(1)} />
+                                </Tabs>
+                                <TabPanel value={opciones} index={0}>
+                                    <Box display={'flex'} justifyContent={'center'}>
+                                        <Typography variant='h3'>Iniciar Sesión</Typography>
+                                    </Box>
                                     <TextField 
-                                        label="Usuario" 
-                                        variant='outlined' 
-                                        sx={{width:"60%", marginTop:"3%"}}
+                                        label='Usuario'
+                                        onChange={(event)=>{ setUsuario(event.target.value);}}
+                                        sx={{width:"94%",mt:5,ml:"3%",mr:"3%"}}
                                     />
-                                    <FormControl 
-                                        variant={"outlined"}  
-                                        sx={{width:"60%", marginTop:"3%"}}
-                                    >
-                                        <InputLabel htmlFor="outlined-adornment-password">Contraseña</InputLabel>
+                                    <FormControl variant="outlined" sx={{width:"94%",mt:5,ml:"3%",mr:"3%"}}>
+                                        <InputLabel 
+                                            htmlFor="outlined-adornment-password" 
+                                        >
+                                            Contraseña
+                                        </InputLabel>
                                         <OutlinedInput
-                                            id="outlined-adornment-password"
-                                            type={showPassword ? 'text' : 'password'}
+                                            type={vercontrasenia ? 'text' : 'password'}
                                             endAdornment={
                                                 <InputAdornment position="end">
                                                     <IconButton
@@ -87,36 +152,39 @@ export function LoginRK(){
                                                         onClick={handleClickShowPassword}
                                                         onMouseDown={handleMouseDownPassword}
                                                         edge="end"
-                                                        >
-                                                        {showPassword ? <VisibilityOff /> : <Visibility />}
+                                                    >
+                                                    {vercontrasenia ? <VisibilityOff /> : <Visibility />}
                                                     </IconButton>
                                                 </InputAdornment>
                                             }
-                                            label={"Contraseña"}
+                                            label="Contrasenia"
+                                            onChange={(event) => { setContrasenia(event.target.value);}}
                                         />
                                     </FormControl>
-                                </ThemeProvider>
+                                    <Box display={'flex'} alignItems={'center'} justifyContent={'space-between'} sx={{mt:5,ml:"3%",mr:"3%"}}>
+                                        <Button 
+                                            variant='contained'
+                                            onClick={submit} 
+                                        >
+                                            Ingresar
+                                        </Button>
+                                        <Link href="" underline="none">Olvidaste tu contraseña</Link>
+                                    </Box>
+                                </TabPanel>
+                                <TabPanel value={opciones} index={1}>
+                                    <Box display={'flex'} justifyContent={'center'}>
+                                        <Typography variant='h3'>Registrarse</Typography>
+                                    </Box>
+                                </TabPanel>
                             </Box>
-                            <Box
-                                display={"flex"}
-                                alignItems={"center"}
-                                justifyContent={"space-between"}
-                                marginTop={2}
-                                paddingBottom={5}
-                                width={"60%"}
-                                marginLeft={"20%"}
-                            >
-                                <ThemeProvider theme={Purple}>
-                                    <Button variant='contained'>Ingresar</Button>
-                                </ThemeProvider>
-                                <ThemeProvider theme={Gray}>
-                                    <Link href="#" underline="hover">olvidaste tu contraseña</Link>
-                                </ThemeProvider>
-                            </Box>
-                        </Box>
+                        </Card>
                     </Box>
                 </Grid>
             </Grid>
         </Box>
     );
 }
+
+LoginRK.propTypes = {
+    setToken: PropTypes.func.isRequired
+};
